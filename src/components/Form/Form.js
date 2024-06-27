1f5f3309e82cf34d4f0adefc5a9a686c56216247
John Gray <john.edwin.gray@gmail.com>
2024-06-27 15:18:41 +1200

    small refactors and notes added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React from 'react'
 
 function Form() {
-    const [ guess, setGuess ] = React.useState('')
-    const handleSubmit = (evt) => {
-      evt.preventDefault()
-      let submittedGuess = guess.toUpperCase()
-      console.log(submittedGuess)
-      setGuess('')
-    }
+  const [guess, setGuess] = React.useState('')
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    console.log({ guess })
+    setGuess('')
+  }
 
-    return (
-      <form className='guess-input-wrapper' onSubmit={handleSubmit}>
-        <label htmlFor='guess-input'>Enter guess:</label>
-        <input
-          name='guess-input' 
-          type='text'
-          value={guess}
-          onChange={(evt) => setGuess(evt.target.value)}
-          pattern=".{5,5}"
-        />
-      </form>  
-  );
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        name="guess-input"
+        type="text"
+        value={guess}
+        onChange={(evt) => {
+          const nextGuess = evt.target.value.toUpperCase()
+          setGuess(nextGuess)
+        }}
+        required
+        pattern="[a-zA-Z]{5}"
+        // for better validation messages
+        title="5 letter word"
+        // the below are redundant currently.
+        // the 2 lines above prevent a weird quirk with minLength
+        // that might be fixed sometime in the future
+        minLength={5}
+        maxLength={5}
+      />
+    </form>
+  )
 }
 
-export default Form;
+export default Form
